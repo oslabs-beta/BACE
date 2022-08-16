f363325dc6e3809afe0f88f0dfc808d550ed32aa
emilybae1705 <baemily22@gmail.com>
2022-08-16 08:19:03 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    path.resolve('./src/index.js')
+  ],
+  output: {
+    path: path.resolve('./build/'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx/,
+        exclude: /node_modules/,
+        include: path.resolve('./src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ]
+          },
+        },
+      },
+      {
+        test: /\.scss$/, 
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+      },
+      {
+        test: /\.css$/, 
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      }
+      // {
+      //   test: /\.s?css$/,
+      //   use: [
+      //     {loader: 'style-loader'},
+      //     {
+      //       loader: 'css-loader?importLoaders=1',
+      //       query: {
+      //         modules: true,
+      //         localIdentName: '[name]__[local]___[hash:base64:5]'
+      //       }
+      //     },
+      //     {loader: 'sass-loader'},
+      //     {
+      //       loader: 'postcss-loader',
+      //       options: {
+      //         plugins: function () {
+      //           return [
+      //             require('postcss-cssnext')
+      //           ];
+      //         }
+      //       }
+      //     }
+      //   ]
+      // }
+    ]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    // fallback to root for other urls
+    historyApiFallback: true,
+    static: {
+      // match the output path
+      directory: path.resolve('./build/'),
+      // match the output 'publicPath'
+      publicPath: '/build',
+    },
+    proxy: {
+      '/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.css']
+  }
+};
