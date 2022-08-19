3a048293cab41f213e1ef97e370e1406ae109dd7
thelucidcoder <thelucidcoder@protonmail.org>
2022-08-19 00:09:49 -0400

    Popup renders, but react is causing uncaught error

--- client/webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const {merge} = require('webpack-merge');
-const config = require('./webpack.config.js')
-
-//merge existing configuration 
-module.exports = merge(config, {
-  mode: 'development',
-  devtool: 'inline-source-map'
-})
--- client/webpack.prod.js
+++
@@ -1,7 +0,0 @@
-const {merge} = require('webpack-merge');
-const config = require('./webpack.config.js')
-
-//merge existing configuration 
-module.exports = merge(config, {
-  mode: 'production'
-})
