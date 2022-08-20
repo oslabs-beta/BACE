93a39bcf5e2208c356e94aa4f23399ee99394141
Katie Ahn <ahoymateykatie@Katies-MacBook-Pro.local>
2022-08-20 11:02:31 -0700

    remove node_modules from git repo

--- extension/node_modules/@sinclair/typebox/value/cast.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Types from '../typebox';
-export declare class ValueCastUnknownTypeError extends Error {
-    readonly schema: Types.TSchema;
-    constructor(schema: Types.TSchema);
-}
-export declare namespace ValueCast {
-    function Visit(schema: Types.TSchema, references: Types.TSchema[], value: any): any;
-    function Cast<T extends Types.TSchema, R extends Types.TSchema[]>(schema: T, references: [...R], value: any): Types.Static<T>;
-}
--- extension/node_modules/react-chartjs-2/dist/chart.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ForwardedRef, ChartProps } from './types';
-export declare const Chart: <TType extends keyof import("chart.js").ChartTypeRegistry = keyof import("chart.js").ChartTypeRegistry, TData = import("chart.js/types/utils").DistributiveArray<import("chart.js").ChartTypeRegistry[TType]["defaultDataPoint"]>, TLabel = unknown>(props: ChartProps<TType, TData, TLabel> & {
-    ref?: ForwardedRef<import("./types").ChartJSOrUndefined<TType, TData, TLabel>> | undefined;
-}) => JSX.Element;
-//# sourceMappingURL=chart.d.ts.map
