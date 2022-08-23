c31079c2eace3382f360dbff09c85908a8ffc54d
Alyssa Chew <alyssachew@MacBook-Pro-2.local>
2022-08-23 10:59:49 -0700

    added injection file

---
+++ extension/src/client/app/injection.ts
@@ -0,0 +1,32 @@
+import utils from '../content/utils.js';
+import TransformControls from '../content/TransformControls.js';
+import EntityCache from '../content/EntityCache.js';
+import ThreeDevTools from '../content/ThreeDevTools';
+import DevToolsScene from '../content/DevToolsScene';
+// import InstrumentedToJSON from '../content/toJSON';
+import THREE from 'three';
+
+const version = chrome.runtime.getManifest().version;
+const red = 'rgb(255, 137, 137)';
+const green = 'rgb(190, 251, 125)'
+const blue = 'rgb(120, 250, 228)';
+
+export default `
+console.log('%c▲%cthree-devtools%cv${version}',
+  'font-size:150%; color:${green}; text-shadow: -10px 0px ${red}, 10px 0px ${blue}; padding: 0 15px 0 10px;',
+  'font-size: 110%; background-color: #666; color:white; padding: 0 5px;',
+  'font-size: 110%; background-color: ${blue}; color:#666; padding: 0 5px;');
+(() => {
+  const DEBUG = false;
+  const utils = (${utils})();
+  const THREE = (${THREE})();
+  (${TransformControls})(THREE);
+  const DevToolsScene = (${DevToolsScene})(THREE);
+  const EntityCache = (${EntityCache})();
+  const devtools = new (${ThreeDevTools})(window.__THREE_DEVTOOLS__);
+  window.__THREE_DEVTOOLS__.dispatchEvent(new CustomEvent('devtools-ready'));
+})();
+`;
+
+  // const InstrumentedToJSON = (${InstrumentedToJSON})(); was originally inside of the export statement -- can add back if we need this toJSON file
+
