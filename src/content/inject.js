72f9ee7bc6f59d636d7faeb1b04811be09064974
thelucidcoder <thelucidcoder@protonmail.org>
2022-09-06 12:37:41 -0400

    testing mv2 to mv3 conversion

---
+++ src/content/inject.js
@@ -0,0 +1,40 @@
+
+(() => {
+/**
+ * This script injected by the installed three.js developer
+ * tools extension.
+ * https://github.com/threejs/three-devtools
+ */
+
+const $devtoolsReady = Symbol('devtoolsReady');
+const $backlog = Symbol('backlog');
+
+// The __THREE_DEVTOOLS__ target is small and light-weight, and collects
+// events triggered until the devtools panel is ready, which is when
+// the events are flushed.
+const target = new class ThreeDevToolsTarget extends EventTarget {
+  constructor() {
+    super();
+    this[$devtoolsReady] = false;
+    this[$backlog] = [];
+    this.addEventListener('devtools-ready', e => {
+      this[$devtoolsReady] = true;
+      for (let event of this[$backlog]) {
+        this.dispatchEvent(event);
+      }
+    }, { once: true });
+  }
+
+  dispatchEvent(event) {
+    if (this[$devtoolsReady] || event.type === 'devtools-ready') {
+      super.dispatchEvent(event);
+    } else {
+      this[$backlog].push(event);
+    }
+  }
+}
+
+Object.defineProperty(window, '__THREE_DEVTOOLS__', {
+  value: target,
+});
+})();
--- src/extension/contentScript.js
+++ src/extension/contentScript.js
@@ -1,52 +1,28 @@
 // Use `text` instead of `src` to get around Chromium bug of execution order
 // when using `src`, resulting in a race condition.
 // https://bugs.chromium.org/p/chromium/issues/detail?id=634381#c3
-const script = document.createElement('script');
-script.text = `
-(() => {
-/**
- * This script injected by the installed three.js developer
- * tools extension.
- * https://github.com/threejs/three-devtools
- */
 
-const $devtoolsReady = Symbol('devtoolsReady');
-const $backlog = Symbol('backlog');
 
-// The __THREE_DEVTOOLS__ target is small and light-weight, and collects
-// events triggered until the devtools panel is ready, which is when
-// the events are flushed.
-const target = new class ThreeDevToolsTarget extends EventTarget {
-  constructor() {
-    super();
-    this[$devtoolsReady] = false;
-    this[$backlog] = [];
-    this.addEventListener('devtools-ready', e => {
-      this[$devtoolsReady] = true;
-      for (let event of this[$backlog]) {
-        this.dispatchEvent(event);
-      }
-    }, { once: true });
+function injectScript(file_path) {
+  const node = document.getElementsByTagName('body');
+  console.log(node)
+  console.log(node[0])
+  //console.log(node.0)
+  //console.log(node[0]);
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', file_path);
+  script.onload = () => {
+    script.parentNode.removeChild(script);
   }
-
-  dispatchEvent(event) {
-    if (this[$devtoolsReady] || event.type === 'devtools-ready') {
-      super.dispatchEvent(event);
-    } else {
-      this[$backlog].push(event);
-    }
-  }
+  node.appendChild(script);
 }
+//the content script is going to be listen to messages from the injected script since its now apart of the page
+// getURL gets us a file within the extension we created
+injectScript(chrome.runtime.getURL('../content/inject.js'));
 
-Object.defineProperty(window, '__THREE_DEVTOOLS__', {
-  value: target,
-});
-})();
-`;
-script.onload = () => {
-  script.parentNode.removeChild(script);
-}
-(document.head || document.documentElement).appendChild(script);
+
+// (document.head || document.documentElement).appendChild(script);
 
 window.addEventListener('message', e => {
   if (e.source !== window ||
